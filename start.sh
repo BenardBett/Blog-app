c057e7374420e78327a5783e3b4274fdf6942e76
BenardBett <kiplangatbett5@gmail.com>
2021-01-05 20:33:33 +0300

    debug the python files

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME='enookimuu@gmail.com'
+export MAIL_PASSWORD='33254733E'
+export SECRET_KEY='mine'
+export DATABASE_URL='postgresql+psycopg2://moringa:Access@localhost/blog'
+
+python3.8 manage.py server
